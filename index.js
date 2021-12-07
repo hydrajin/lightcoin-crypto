088797a87e7b38101ae5e0f2e74ca1ca1b90e93b
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-07 02:02:53 -0500

    Need to implement fuinishing features

--- index.js
+++ index.js
@@ -1,29 +1,108 @@
-let balance = 500.00;
+// ! LightCoin (LHL)
 
-class Withdrawal {
+// ? Features:
+/*
+Here's a list of features that our code needs to support:
 
-  constructor(amount) {
+Allow multiple accounts to be created
+Each account can have many transactions
+Allow withdrawals and deposits into accounts
+Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+Allow us to retrieve the current balance of the account at any time
+Don't allow withdrawals that exceed the remaining balance of the account
+*/
+
+// let balance = 500.00;
+
+// create an account class to keep track of user and their balance
+// Now we can create a new account for every user of the app!!!
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have account balance start at zero ($0)
+    this.balance = 0;
+  }
+}
+
+
+// Create a Transaction class (Has withdrawl and deposit sublass)
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
-
 }
 
+class Withdrawal extends Transaction {
+
+  // Modified
+
+  // commit() {
+  get value() {
+    // this.account.balance -= this.amount;
+    // by putting commit() in the transaction class
+    return -this.amount;
+    // look at that refactor!
+  }
+}
+// create a withdrawl class!
+
+class Deposit extends Transaction {
+
+  // Modified
+
+  // commit() {
+  get value() {
+    return this.amount;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.commit();
+// console.log('Transaction 1:', t1);
+
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log('Transaction 2:', t2);
+
+// t3 = new Deposit(120.00);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+// t1 = new Withdrawal(50.25, myAccount);
+// t1.value();
+// console.log('Transaction 1:', t1);
+
+const myAccount = new Account("Cotton Eye Joe");
+
+console.log(`Welcome, ${myAccount.username}\nHere is your account summary:`);
+console.log(`-----------------------------`);
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(500.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(350.25, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+const t3 = new Deposit(865.65, myAccount);
+t3.commit();
+
+
+console.log('Ending Balance:', myAccount.balance);
+
+
+// *! This pattern is called Dependency Injection. It's a fancy word that simply means "passing an object the things it needs rather
+// *!than having the object access them itself". It makes for code that is much more modular and testable.
